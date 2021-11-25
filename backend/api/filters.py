adce07eb299eface438652a307dc748bef1a039a
Gene Koro <eug.korobkov@gmail.com>
2021-11-25 19:44:42 +0300

    Recipe tag filtering improved

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django_filters.rest_framework import FilterSet, filters
 from rest_framework.filters import SearchFilter
 
-from .models import Recipe
+from .models import Recipe, Tag
 
 User = get_user_model()
 
@@ -12,7 +12,11 @@ class IngredientNameSearchFilter(SearchFilter):
 
 
 class RecipeAuthorAndTagFilter(FilterSet):
-    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all()
+    )
     author = filters.ModelChoiceFilter(
         queryset=User.objects.all()
     )
